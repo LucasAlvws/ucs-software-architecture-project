4f42eae36518c232122aaa29dfb1675aece2332b
Lucas Alves <sociallucasalves@gmail.com>
2025-04-25 21:09:19 -0300

    wip: mudando pra hibrido requests

--- app/models/course.py
+++ app/models/course.py
@@ -3,7 +3,6 @@ from app.main import db
 
 class Course(db.Model):
     __tablename__ = 'courses'
-    __table_args__ = (db.UniqueConstraint('detailed_name', 'modality', 'degree', name='unique_course'),)
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), nullable=False)
     detailed_name = db.Column(db.String(64))
--- app/models/institution.py
+++ app/models/institution.py
@@ -3,7 +3,6 @@ from app.main import db
 
 class Institution(db.Model):
     __tablename__ = 'institutions'
-    __table_args__ = (db.UniqueConstraint('name', 'organization', 'category', name='unique_institution'),)
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(128))
     acronym = db.Column(db.String(64))
